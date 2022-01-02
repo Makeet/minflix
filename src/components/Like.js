0019ae2b434c73e2317ef752fd0a4cbc8b589b64
recflow <chjh9107@gmail.com>
2022-01-02 20:54:52 +0900

    화면에 이미지 띄우기

--- src/components/Like.js
+++ src/components/Like.js
@@ -5,35 +5,44 @@ function Like() {
   const [movies, setMovies] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  console.log("like db call...");
+
   const apiCallMovies = async () => {
     setError(null);
     setLoading(true);
     setMovies(null);
+    console.log("like db call...");
     await axios
       .get("/api/like")
       .then((response) => {
-        console.log(response.data);
+        //console.log(response.data);
         setMovies(response.data);
       })
       .catch((error) => {
         console.log("E>>>", error);
+        setError(error);
       });
+    setLoading(false);
+  
   };
+  console.log(movies)
   useEffect(() => {
     apiCallMovies();
-  }, []);
-  if(loading) return <div>로딩중...</div>
-  if(error) return <div>에러발생</div>
-  if(!movies) return null;
-  return (<>
-  <ul>
-    {movies.map(movie=>(
-      <li key={movies.mno}>
-        {movie.title}
-      </li>
-    ))}
-  </ul>
-  </>);
+  },[]);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러발생</div>;
+  if (!movies) return null;
+  return (
+    <div>
+      <h1>좋아요 누른 작품</h1>
+      <ul>
+        {movies.data.map((movie) => (
+          <li key={movie.mno} class="item">
+            <img src={movie.poster} alt={`poster of ${movie.mno}`} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default Like;
