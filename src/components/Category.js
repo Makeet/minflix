32779cda05906f01c04a56ef10f928570b02f4b5
recflow <chjh9107@gmail.com>
2022-01-05 15:20:17 +0900

    좋아요 누른 작품

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import categoryData from "DB/CategoryData.json";
 
@@ -9,7 +8,6 @@ function Category() {
   const toggleCategory = () => {
     setToggleCategory((isOpenCategory) => !isOpenCategory);
   };
-  /* 추후 수정 예정 api추가하여 fetch로 받아온 다음 작업해야함 */
 
   const checkItemHandler = (checked, id) => {
     if (checked) {
@@ -19,34 +17,29 @@ function Category() {
     }
   };
 
-  /*
-   *  Category를 3개로 나눠서 표 형식으로 적용해 줄 것
-   *    가설 1 : Pagination 이용하면 가능하지 않을까???
-   *    - li 태그 안에 div 넣고
-   *    - ul 3개 들어가도록
-   */
-  const genre = categoryData.category.map((item, index) => {
-    return (
-      <>
-        <li key={"category" + index}>
-          <input
-            type="checkbox"
-            id={item}
-            checked={checkedItem.includes(item) ? true : false}
-            onChange={(e) => checkItemHandler(e.currentTarget.checked, item)}
-          />
-          {item}
-        </li>
-      </>
-    );
-  });
   return (
     <>
       <li key="categories">
-        <a onClick={() => toggleCategory()}>카테고리</a>
+        <div onClick={() => toggleCategory()}>카테고리</div>
         {isOpenCategory ? (
           <div className="category">
-            <ul className="like-table">{genre}</ul>
+            <ul className="like-table">
+              {categoryData.category.map((item, index) => {
+                return (
+                  <li key={"category" + index}>
+                    <input
+                      type="checkbox"
+                      id={item}
+                      checked={checkedItem.includes(item) ? true : false}
+                      onChange={(e) =>
+                        checkItemHandler(e.currentTarget.checked, item)
+                      }
+                    />
+                    {item}
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         ) : (
           ""
--- src/components/Like.js
+++ src/components/Like.js
@@ -10,39 +10,48 @@ function Like() {
     setError(null);
     setLoading(true);
     setMovies(null);
-    console.log("like db call...");
+
     await axios
       .get("/api/like")
       .then((response) => {
-        //console.log(response.data);
         setMovies(response.data);
       })
       .catch((error) => {
-        console.log("E>>>", error);
         setError(error);
       });
     setLoading(false);
-  
   };
-  console.log(movies)
   useEffect(() => {
     apiCallMovies();
-  },[]);
+  }, []);
 
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러발생</div>;
   if (!movies) return null;
   return (
-    <div>
-      <h1>좋아요 누른 작품</h1>
-      <ul>
-        {movies.data.map((movie) => (
-          <li key={movie.mno} class="item">
-            <img src={movie.poster} alt={`poster of ${movie.mno}`} />
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="sub-header">
+        <h2 className="row-header">좋아요 누른 작품</h2>
+      </div>
+      <div className="favorite">
+        <div className="row-container">
+          {movies.data.map((movie) => (
+            <div key={`keynum${movie.mno}`} className="item">
+              <div className="boxart-size-16x9 boxart-container boxart-rounded">
+                <img
+                  className="boxart-image-in-padded-container"
+                  src={movie.poster}
+                  alt={`poster of ${movie.mno}`}
+                />
+                <div className="fallback-text-container">
+                  <p className="fallback-text">{`${movie.title}`}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 export default Like;
